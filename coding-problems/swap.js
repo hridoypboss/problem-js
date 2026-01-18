392740d14d4de75b089d2a14d08a424d06c2fe34
hridoypboss <hridoydakua1994@gmail.com>
2026-01-18 22:02:32 +0600

    first commit

---
+++ coding-problems/swap.js
@@ -0,0 +1,8 @@
+// Swap the values of two variables
+var a = 5;
+var b = 7;
+var temp = a;       // temp বিতর a এর মান সংরক্ষণ করো হয়েছে
+a = b;             // a এর মান b এর মান দিয়ে প্রতিস্থাপন করো হয়েছে
+b = temp;         // b এর মান temp এর মান দিয়ে প্রতিস্থাপন করো হয়েছে
+
+console.log("After swap: a =", a, "b =", b);    // Output: After swap: a = 7 b = 5
